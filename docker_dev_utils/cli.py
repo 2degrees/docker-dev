00ea8d08c717cef8e307a070378119906b179764
Gustavo Narea <gustavonarea@2degreesnetwork.com>
2016-08-11 15:37:50 +0100

    Reimplemented calculation of fallback project name and path
    INF-5

--- docker_dev_utils/cli.py
+++ docker_dev_utils/cli.py
@@ -1,49 +1,57 @@
-from os import getcwd
-from os.path import basename
+from os.path import basename, abspath
 
 import click
 
-from docker_dev_utils.exceptions import ConfigurationError
 from docker_dev_utils._logging import handle_callback_exception
+from docker_dev_utils.exceptions import VCSError
 from docker_dev_utils.projects import uninstall_project, install_project
 from docker_dev_utils.vcs import get_repository_info_from_path
 
 
-_CURRENT_WORKDIR = getcwd()
-try:
-    _CURRENT_PROJECT_INFO = get_repository_info_from_path(_CURRENT_WORKDIR)
-except ConfigurationError:
-    _DEFAULT_PROJECT_PATH = None
-    _DEFAULT_PROJECT_NAME = None
-else:
-    _DEFAULT_PROJECT_PATH = _CURRENT_PROJECT_INFO.path
-    _DEFAULT_PROJECT_NAME = '{}-{}'.format(
-        basename(_DEFAULT_PROJECT_PATH),
-        _CURRENT_PROJECT_INFO.branch_name,
-    )
+def _convert_click_path_arg_to_absolute(ctx, param, value):
+    return abspath(value)
+
+
+def _calculate_default_project_name(ctx, param, value):
+    if value:
+        project_name = value
+    else:
+        project_path = ctx.params['project_path']
+        branch_name = _get_project_branch_name(project_path)
+        project_name = '{}-{}'.format(basename(project_path), branch_name)
+    return project_name
+
+
+def _get_project_branch_name(project_path):
+    try:
+        project_info = get_repository_info_from_path(project_path)
+    except VCSError:
+        branch_name = ''
+    else:
+        branch_name = project_info.branch_name
+    return branch_name
 
 
 @click.group()
 @click.option(
     '--project-path',
-    default=_DEFAULT_PROJECT_PATH,
+    default='.',
     show_default=True,
+    callback=_convert_click_path_arg_to_absolute,
+    is_eager=True,
 )
-@click.option(
-    '--project-name',
-    default=_DEFAULT_PROJECT_NAME,
-    show_default=True,
-)
-@click.pass_context
+@click.option('--project-name', callback=_calculate_default_project_name)
 @handle_callback_exception
-def main(context, project_path, project_name):
+def main(project_path, project_name):
     # TODO: Should we require the path to docker-compose.yml instead and derive the project_path from it?
-    # TODO: Make the `project_path` absolute
-    context.obj = {'project_path': project_path, 'project_name': project_name}
+    pass
+
+
 
 
 @main.command()
-@click.pass_obj
+@click.pass_context
 @handle_callback_exception
-def clean(obj):
-    uninstall_project(obj['project_path'], obj['project_name'])
+def clean(context):
+    main_args = context.parent.params
+    uninstall_project(main_args['project_path'], main_args['project_name'])
